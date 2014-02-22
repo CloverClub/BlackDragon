e47f3ef3ff3a2506fa90c887c7f89bdedd8280f5
rookie <13bigrookie13@gmail.com>
2014-02-22 14:14:39 +0200

    Add PlayField

---
+++ Model/PlayField.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class PlayField
+    {
+        private int width = 80;
+        private int height = 50;
+
+        public int Width
+        {
+            get { return width; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+        }
+
+        public PlayField()
+        { 
+        }
+    }
+}
