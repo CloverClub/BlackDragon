46e22527707efb35a0fcb58cad793178e9071525
Georgi Borisov <gborisov90@gmail.com>
2014-02-22 18:57:12 +0200

    Added Spearman and Dwarf, as Heroes.

---
+++ Model/Heroes/Dwarf.cs
@@ -0,0 +1,14 @@
+﻿namespace Model
+{
+    public class Dwarf : Hero
+    {
+
+        public Dwarf(string name, int exp, int health, int damage)
+        {
+            this.Name = name;
+            this.Experience = exp;
+            this.HealthPoints = health;
+            this.DamagePoints = damage;
+        }
+    }
+}
---
+++ Model/Heroes/Spearman.cs
@@ -0,0 +1,14 @@
+﻿namespace Model
+{
+    public class Spearman : Hero
+    {
+
+        public Spearman(string name, int exp, int health, int damage)
+        {
+            this.Name = name;
+            this.Experience = exp;
+            this.HealthPoints = health;
+            this.DamagePoints = damage;
+        }
+    }
+}
