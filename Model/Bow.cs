73d128267c40f981749d91a15a84bbabb08c8813
Georgi Borisov <gborisov90@gmail.com>
2014-02-22 15:04:46 +0200

    Added range to class Bow : Weapon

--- Model/Bow.cs
+++ Model/Bow.cs
@@ -7,6 +7,11 @@
             get { return this.addDamage; }
             set { this.addDamage = value; }
         }
+        public int Range
+        {
+            get { return this.range; }
+            set { this.range = value; }
+        }
         public override void DrawWeapon()
         {
             throw new System.NotImplementedException();
