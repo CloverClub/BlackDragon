e8c03ecee619ca100010aacaece46551766b2740
Georgi Borisov <gborisov90@gmail.com>
2014-02-22 12:15:31 +0200

    Adding Main Method

---
+++ Model/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class MainMethod
+    {
+        public static void Main()
+        {
+            Archer newarcher = new Archer();
+            newarcher.Experience = 100;
+        }
+    }
+}
