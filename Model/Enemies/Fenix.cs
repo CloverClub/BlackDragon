3ce23c313237236008b42c309cd3901e05eec8a8
Georgi Borisov <gborisov90@gmail.com>
2014-02-22 19:58:10 +0200

    Added Yeti, Fenix and Brute as classes of enemies

---
+++ Model/Enemies/Fenix.cs
@@ -0,0 +1,23 @@
+﻿namespace Model
+{
+    public class Fenix : Enemy
+    {
+        public int Damage
+        {
+            get { return this.damagePoints; }
+            set { this.damagePoints = value; }
+        }
+
+        public int Health
+        {
+            get { return this.healthPoints; }
+            set { this.healthPoints = value; }
+        }
+
+        public Fenix(int health, int damage)
+        {
+            this.Health = health;
+            this.Damage = damage;
+        }
+    }
+}
