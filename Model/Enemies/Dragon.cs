96fbd26629f1a6ce330999dd62dde48bf3d3a81e
Georgi Borisov <gborisov90@gmail.com>
2014-02-23 15:02:33 +0200

    Draw & Erase enemy Dragon

--- Model/Enemies/Dragon.cs
+++ Model/Enemies/Dragon.cs
@@ -1,4 +1,6 @@
-﻿namespace Model
+﻿using System;
+
+namespace Model
 {
     public class Dragon : Enemy
     {
@@ -18,6 +20,51 @@
         {
             this.Health = health;
             this.Damage = damage;
+        }
+
+        public override void Draw()
+        {
+            int x = Position.Left;
+            int y = Position.Top;
+
+            Console.SetCursorPosition(Position.Left, Position.Top);
+            Console.WriteLine("   (_(");
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(@"  ('')");
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(@"_  ''\ )>,_     .-->");
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(@"_>--w/((_ >,_.'");
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(@"       ///");
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(@"      ''`''");
+            //
+            //   (_(
+            //  ('')
+            //_  "\ )>,_     .-->
+            //_>--w/((_ >,_.'
+            //       ///
+            //       "`"
+
+        }
+
+        public override void Erase()
+        {
+            int y = Position.Top;
+
+            Console.SetCursorPosition(Position.Left, Position.Top);
+            Console.WriteLine(new string(' ', this.Width));
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(new string(' ', this.Width));
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(new string(' ', this.Width));
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.Write(new string(' ', this.Width));
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.Write(new string(' ', this.Width));
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.Write(new string(' ', this.Width));
         }
     }
 }
