ec2d49a43130f7e9ce5ac616e464a4e17ab1d9ce
rookie <13bigrookie13@gmail.com>
2014-02-23 16:08:10 +0200

    Update Yeti and Zombie vizualization

--- Model/Enemies/Yeti.cs
+++ Model/Enemies/Yeti.cs
@@ -1,4 +1,6 @@
-﻿namespace Model
+﻿using System;
+
+namespace Model
 {
     public class Yeti : Enemy
     {
@@ -18,6 +20,46 @@
         {
             this.Health = health;
             this.Damage = damage;
+        }
+
+        public override void Draw()
+        {
+            int x = Position.Left;
+            int y = Position.Top;
+
+            Console.SetCursorPosition(Position.Left, Position.Top);
+            Console.WriteLine(@" __("")__");
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(@"( (   ) )");
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(@"/ |   | \");
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(@"\( ( ) )/");
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(@" /_| |_\");
+            // __(")__
+            //( (   ) )
+            /// |   | \
+            //\( ( ) )/
+            // /_| |_\
+
+
+        }
+
+        public override void Erase()
+        {
+            int y = Position.Top;
+
+            Console.SetCursorPosition(Position.Left, Position.Top);
+            Console.WriteLine(new string(' ', this.Width));
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(new string(' ', this.Width));
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(new string(' ', this.Width));
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(new string(' ', this.Width));
+            Console.SetCursorPosition(Position.Left, ++y);
+            Console.WriteLine(new string(' ', this.Width));
         }
     }
 }
